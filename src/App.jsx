8e91a3337f4bbc90dfb7b1e2bd4532ed8b4ddeb4
Jhonatan Carreazo <142127117+jhonatancarreazo@users.noreply.github.com>
2024-11-28 00:06:18 -0500

    Corrección de sintaxis en  App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Blog from './components/Blog'
+import Blog from './components/Blog';
 import { BrowserRouter as Router,Route, Routes,Navigate  } from 'react-router-dom';
 // import './App.css';
 
